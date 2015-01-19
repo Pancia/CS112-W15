9b6df2984bb05865c4dad557bd5a717450b09e78
Pancia <anthony.dayzerostudio@gmail.com>
2015-01-19 03:25:17 -0800

    Added largest test case possible.

--- hw2/hw2_test.hs
+++ hw2/hw2_test.hs
@@ -26,6 +26,19 @@ hw2Tests =
         ,sayNum "5" ~?= "five "
         ,sayNum "23" ~?= "twenty three "
         ,sayNum "230" ~?= "two hundred thirty "
+        ,sayNum "13016" ~?= "thirteen thousand sixteen "
         ,sayNum "82379" ~?= "eighty two thousand three hundred seventy nine "
         ,sayNum "93218065" ~?= "ninety three million two hundred eighteen thousand sixty five "
+        ,sayNum "298732619826139659876129872356978162498713957197613698162491871352" ~?=
+        "two hundred ninety eight vigintillion seven hundred thirty two novemdecillion " ++
+        "six hundred nineteen octodecillion eight hundred twenty six septendecillion " ++
+        "one hundred thirty nine sexdecillion six hundred fifty nine quindecillion " ++
+        "eight hundred seventy six quattuordecillion one hundred twenty nine tredecillion " ++
+        "eight hundred seventy two duodecillion three hundred fifty six undecillion " ++
+        "nine hundred seventy eight decillion one hundred sixty two nonillion " ++
+        "four hundred ninety eight octillion seven hundred thirteen septillion " ++
+        "nine hundred fifty seven sextillion one hundred ninety seven quintillion " ++
+        "six hundred thirteen quadrillion six hundred ninety eight trillion " ++
+        "one hundred sixty two billion four hundred ninety one million eight hundred " ++
+        "seventy one thousand three hundred fifty two "
         ]
