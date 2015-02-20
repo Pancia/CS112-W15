3e3915978fa36fe7c3b297f1029080c74af9e7c1
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-02-20 01:46:16 -0800

    Almost done, just missing evalS[_maybe] (For...)

--- hw5/hw5.hs
+++ hw5/hw5.hs
@@ -21,6 +21,10 @@ data Value = IntVal Int       -- Integer value
            | BoolVal Bool     -- Boolean value
            deriving (Eq)
 
+instance Ord Value where
+    (IntVal v1) `compare` (IntVal v2) = v1 `compare` v2
+    _ `compare` _ = error "cant compare BoolVals"
+
 -- Expressions are variables, literal values, unary and binary operations
 data Expression = Var Variable                    -- e.g. x
                 | Val Value                       -- e.g. 2
@@ -29,22 +33,107 @@ data Expression = Var Variable                    -- e.g. x
 
 --store = map var val
 evalE :: Expression -> Store -> (Value, Store)
+evalE (Assignment x v) s = (v', s'')
+    where (v', s') = evalE v s
+          s'' = M.insert x v' s'
 evalE (BinOp op e1 e2) s = (applyOp op e1' e2', s'')
     where (e1', s')  = evalE e1 s
           (e2', s'') = evalE e2 s'
 evalE (Var x) s = (x', s)
     where x' = fromMaybe (error "not found") $ M.lookup x s
 evalE (Val v) s = (v, s)
-evalE (Assignment x v) s = (v', s'')
-    where (v', s') = evalE v s
-          s'' = M.insert x v' s'
+
+evalE_maybe :: Expression -> Store -> Maybe (Value, Store)
+evalE_maybe (Assignment x v) s = do (v', s') <- evalE_maybe v s
+                                    return (v', M.insert x v' s')
+evalE_maybe (BinOp o a b) s = do (a',s') <- evalE_maybe a s
+                                 (b',s'') <- evalE_maybe b s'
+                                 return (applyOp o a' b', s'')
+evalE_maybe (Var x) s = do x' <- M.lookup x s
+                           return (x', s)
+evalE_maybe (Val v) s = Just (v, s)
+
+--Imperative a = Store -> Maybe (a, Store)
+evalE_monad :: Expression -> Imperative Value
+evalE_monad (BinOp o a b) = do a' <- evalE_monad a
+                               b' <- evalE_monad b
+                               return (applyOp o a' b')
+evalE_monad (Var x) = getVar x
+evalE_monad (Val v) = return v
+evalE_monad (Assignment x e) = do e' <- evalE_monad e
+                                  setVar x e'
+
+getVar :: Variable -> Imperative Value
+getVar var = Imperative (\store -> (M.lookup var store >>= (\v -> Just (v,store))))
+
+setVar :: Variable -> Value -> Imperative Value
+setVar var val = Imperative (\store -> Just (val, M.insert var val store))
 
 -- Statements are expressions, conditionals, while loops and sequences
 data Statement = Expr Expression                   -- e.g. x = 23
                | If Expression Statement Statement -- if e then s1 else s2 end
                | While Expression Statement        -- while e do s end
                | Sequence Statement Statement      -- s1; s2
+               | For Variable Expression Expression Statement
                | Skip                              -- no-op
+
+evalS :: Statement -> Store -> Store
+evalS w@(While e s1) s = case evalE e s of
+                             (BoolVal True,s')  -> let s'' = evalS s1 s' in evalS w s''
+                             (BoolVal False,s') -> s'
+                             _                  -> error "Condition must be a BoolVal"
+evalS Skip s             = s
+evalS (Expr e) s         = snd $ evalE e s
+evalS (Sequence s1 s2) s = evalS s2 $ evalS s1 s
+evalS (If e s1 s2) s     = case evalE e s of
+                             (BoolVal True,s')  -> evalS s1 s'
+                             (BoolVal False,s') -> evalS s2 s'
+                             _                  -> error "Condition must be a BoolVal"
+evalS f@(For v e1 e2 st) s = undefined
+
+evalS_maybe :: Statement -> Store -> Maybe Store
+evalS_maybe w@(While e s1) s = do x <- evalE_maybe e s
+                                  case x of
+                                      (BoolVal True,s') -> do s'' <- evalS_maybe s1 s'
+                                                              evalS_maybe w s''
+                                      (BoolVal False,s') -> return s'
+                                      _ -> Nothing
+evalS_maybe Skip s = return s
+evalS_maybe (Expr e) s = return . snd =<< evalE_maybe e s
+evalS_maybe (Sequence s1 s2) s = evalS_maybe s2 =<< evalS_maybe s1 s
+evalS_maybe (If e s1 s2) s = do x <- evalE_maybe e s
+                                case x of
+                                    (BoolVal True,s') -> evalS_maybe s1 s'
+                                    (BoolVal False,s') -> evalS_maybe s2 s'
+                                    _ -> Nothing
+
+evalS_monad :: Statement -> Imperative ()
+evalS_monad (While e s1) = do e' <- evalE_monad e
+                              case e' of
+                                  BoolVal True -> evalS_monad s1
+                                  BoolVal False -> return ()
+                                  _             -> error "Condition must be a BoolVal"
+evalS_monad Skip = return ()
+evalS_monad (Sequence s1 s2) = do evalS_monad s1
+                                  evalS_monad s2
+                                  return ()
+evalS_monad (Expr e) = do evalE_monad e
+                          return ()
+evalS_monad (If e s1 s2) = do e' <- evalE_monad e
+                              case e' of
+                                  BoolVal True -> evalS_monad s1
+                                  BoolVal False -> evalS_monad s2
+                                  _             -> error "Condition must be a BoolVal"
+evalS_monad (For v start end body) = do init <- evalE_monad start
+                                        final <- evalE_monad end
+                                        setVar v (applyOp Plus init $ IntVal 1)
+                                        evalS_monad body
+                                        v' <- getVar v
+                                        if (v' < final)
+                                           then evalS_monad (For v (Val v') end body)
+                                           else do setVar v $ applyOp Plus v' $ IntVal 1
+                                                   return ()
+
 
 -- All binary operations
 data Op = Plus         --  +  :: Int -> Int -> Int
@@ -79,6 +168,7 @@ instance Show Statement where
         show (While e s) = "while " ++ show e ++ " do " ++ show s ++ " end"
         show (Sequence s1 s2) = show s1 ++ ";" ++ show s2
         show Skip = "no-op"
+        show (For v e1 e2 s) = "for " ++ v ++ " in " ++ show e1 ++ " to " ++ show e2 ++ " do " ++ show s ++ " end"
 
 -- The `Store` is an associative map from `Variable` to `Value` representing the memory
 type Store = Map Variable Value
@@ -148,17 +238,17 @@ stmtParser1 = (Expr <$> exprParser)
               body <- stmtParser
               P.reserved lexer "end"
               return (While cond body)
-        -- <|> do
-         --    P.reserved lexer "for"
-         --    var <- P.identifier lexer
-         --    P.reserved lexer "in"
-         --    low <- exprParser
-         --    P.reserved lexer "to"
-         --    up <- exprParser
-         --    P.reserved lexer "do"
-         --    body <- stmtParser
-         --    P.reserved lexer "end"
-         --    return (For var low up body)
+          <|> do
+              P.reserved lexer "for"
+              var <- P.identifier lexer
+              P.reserved lexer "in"
+              low <- exprParser
+              P.reserved lexer "to"
+              up <- exprParser
+              P.reserved lexer "do"
+              body <- stmtParser
+              P.reserved lexer "end"
+              return (For var low up body)
 
 -------- Helper functions --------
 
@@ -199,8 +289,18 @@ run parser input eval = case parse parser "" input of
     Left err -> print err
     Right x  -> print (eval x empty)
 
+newtype Imperative a = Imperative {
+    runImperative :: Store -> Maybe (a, Store)
+}
+
+instance Monad Imperative where
+    return a = Imperative (\s -> Just (a,s))
+    b >>= f = Imperative (\s -> do (v1,s1) <- runImperative b s
+                                   runImperative (f v1) s1)
+    fail _ = Imperative $ const Nothing
+
 -- Parse and run the given WHILE programs using monads
---runMonad :: String -> Maybe Store
---runMonad input = proc (parse stmtParser "" input)
---    where proc (Right x) = snd `fmap` runImperative (evalS_monad x) empty
---          proc _         = Nothing
+runMonad :: String -> Maybe Store
+runMonad input = proc (parse stmtParser "" input)
+    where proc (Right x) = snd `fmap` runImperative (evalS_monad x) empty
+          proc _         = Nothing
--- hw5/hw5_test.hs
+++ hw5/hw5_test.hs
@@ -3,7 +3,9 @@ module Hw5Test where
 import Test.HUnit
 import Hw5
 
+import Text.Parsec (parse)
 import Data.Map
+import Data.Either
 
 main :: IO Counts
 main = runTestTT $ TestList tests
@@ -20,183 +22,30 @@ tests = [p' "1+1" ~?= "1 + 1"
         ,r' exprParser "x = 23" evalE ~?= (IntVal 23,fromList [("x",IntVal 23)])
         ,r' exprParser "x = y = 2 + 3" evalE ~?= (IntVal 5,fromList [("x",IntVal 5),("y",IntVal 5)])
       --
-        ,r' stmtParser "x=1+1" evalS ~?= fromList [("x",2)]
-        ,r' stmtParser "x = 2; x = x + 3" evalS ~?= fromList [("x",5)]
-        ,r' stmtParser "if true then x = 1 else x = 2 end" evalS ~?= fromList [("x",1)]
-        ,r' stmtParser "x = 2; y = x + 3; if y < 4 then z = true else z = false end" evalS ~?= fromList [("x",2),("y",5),("z",false)]
-        ,r' stmtParser "x = 1; while x < 3 do x = x + 1 end" evalS ~?= fromList [("x",3)]
-        ,r' stmtParser "x = 1 ; y = 1; while x < 5 do x = x + 1 ; y = y * x end" evalS ~?= fromList [("x",5),("y",120)]
+        ,r' stmtParser "x=1+1" evalS ~?= fromList [("x",IntVal 2)]
+        ,r' stmtParser "x = 2; x = x + 3" evalS ~?= fromList [("x",IntVal 5)]
+        ,r' stmtParser "if true then x = 1 else x = 2 end" evalS ~?= fromList [("x",IntVal 1)]
+        ,r' stmtParser "x = 2; y = x + 3; if y < 4 then z = true else z = false end" evalS ~?= fromList [("x",IntVal 2),("y",IntVal 5),("z",BoolVal False)]
+        ,r' stmtParser "x = 1; while x < 3 do x = x + 1 end" evalS ~?= fromList [("x",IntVal 3)]
+        ,r' stmtParser "x = 1 ; y = 1; while x < 5 do x = x + 1 ; y = y * x end" evalS ~?= fromList [("x",IntVal 5),("y",IntVal 120)]
       --
-      --,run exprParser "1+1" evalE_maybe ~?= Just (2,fromList [])
-      --,run exprParser "10 < x + 1" evalE_maybe ~?= Nothing
-      --,run exprParser "10 == 4 * 2" evalE_maybe ~?= Just (false,fromList [])
-      --,run stmtParser "x = 2; y = z" evalS_maybe ~?= Nothing
-      --,run stmtParser "x = true; if x then y = 1 else y = 2 end" evalS_maybe ~?= Just (fromList [("x",true),("y",1)])
-      --,run stmtParser "x = 1; if x then y = 1 else y = 2 end" evalS_maybe ~?= Nothing
+        ,r' exprParser "1+1" evalE_maybe ~?= Just (IntVal 2,fromList [])
+        ,r' exprParser "10 < x + 1" evalE_maybe ~?= Nothing
+        ,r' exprParser "10 == 4 * 2" evalE_maybe ~?= Just (BoolVal False,fromList [])
+        ,r' stmtParser "x = 2; y = z" evalS_maybe ~?= Nothing
+        ,r' stmtParser "x = true; if x then y = 1 else y = 2 end" evalS_maybe ~?= Just (fromList [("x",BoolVal True),("y",IntVal 1)])
+        ,r' stmtParser "x = 1; if x then y = 1 else y = 2 end" evalS_maybe ~?= Nothing
       --
-      --,runMonad "x = 1" ~?= Just (fromList [("x", 1)])
-      --,runMonad "x = 1; if x == 1 then y = 1 else y = 2 end" ~?= Just (fromList [("x", 1),("y", 1)])
-      --,runMonad "x = 1; if x == z then y = 1 else y = 2 end" ~?= Nothing
-      --,runMonad "while 23 x = x + 1 end" ~?= Nothing
+        ,runMonad "x = 1" ~?= Just (fromList [("x", IntVal 1)])
+        ,runMonad "x = 1; if x == 1 then y = 1 else y = 2 end" ~?= Just (fromList [("x", IntVal 1),("y", IntVal 1)])
+        ,runMonad "x = 1; if x == z then y = 1 else y = 2 end" ~?= Nothing
+        ,runMonad "while 23 x = x + 1 end" ~?= Nothing
       --
-      --,(For "a" (Val (IntVal 1)) (Val (IntVal 2)) (Expr (Val (IntVal 3)))) ~?= "for a in 1 to 2 do 3 end"
-      --,parse stmtParser "" "for x in 1 to 4 do y = x end" ~?= "Right for x in 1 to 4 do y = x end"
-      --,run stmtParser "for x in 1 to 4 do y = x end" evalS ~?= fromList [("x",5),("y",4)]
-      --,run stmtParser "for x in 1 to 4 do y = x end" evalS_maybe ~?= Just (fromList [("x",5),("y",4)])
-      --,run stmtParser "for x in 1 to 4 do y = z end" evalS_maybe ~?= Nothing
-      --,runMonad "for x in 1 to 4 do y = x end" ~?= Just (fromList [("x",5),("y",4)])
-      --,runMonad "for x in 1 to 4 do y = z end" ~?= Nothing
-      ]
-{-
---EX2
-evalE :: Expression -> Store -> (Value, Store)
-
-that takes as input an expression and a store and returns a value. If a variable is not found (e.g. because it is not initialized) throw an error with the error function.
-
-The following case is given to you:
-
-evalE (BinOp o a b) s = (applyOp o a' b', s'')
-    where (a', s')  = evalE a s
-          (b', s'') = evalE b s'
-
-You still have to write the following:
-
-evalE (Var x) s = ...
-evalE (Val v) s = ...
-evalE (Assignment x e) s = ...
-
-Hint: Use Data.Map.lookup symbol map to lookup a variable in the map and Data.Map.insert key value map to insert a variable with the provided key into the map.
-
---EX3
-evalS :: Statement -> Store -> Store
-
-that takes as input a statement and a store and returns a possibly modified store.
-
-The following case is given to you:
-
-evalS w@(While e s1) s = case (evalE e s) of
-                          (BoolVal True,s')  -> let s'' = evalS s1 s' in evalS w s''
-                          (BoolVal False,s') -> s'
-                          _                  -> error "Condition must be a BoolVal"
-
-You still have to write the following
-
-evalS Skip s             = ...
-evalS (Expr e) s         = ...
-evalS (Sequence s1 s2) s = ...
-evalS (If e s1 s2) s     = ...
-
-In the If case, if e evaluates to a non-boolean value, throw an error using the error function.
-
-Examples:
-
---EX4
-We use the Maybe type to deal with cases like uninitialized variables or non-boolean tests instead of throwing a runtime error.
-
-Write the following function that takes an expression and store and returns a Maybe (Value,Store) and never throws a runtime error.
-
-The following case is given to you:
-
-evalE_maybe (BinOp o a b) s = do (a',s') <- evalE_maybe a s
-                                 (b',s'') <- evalE_maybe b s'
-                                 return (applyOp o a' b', s'')
-
-You still have to write:
-
-evalE_maybe :: Expression -> Store -> Maybe (Value, Store)
-evalE_maybe (Var x) s          = ..
-evalE_maybe (Val v) s          = ...
-evalE_maybe (Assignment x e) s = ...
-
-Similarly for statements
-
-evalS_maybe :: Statement -> Store -> Maybe Store
-evalS_maybe (While e s1) s     = ...
-evalS_maybe Skip s             = ...
-evalS_maybe (Sequence s1 s2) s = ...
-evalS_maybe (Expr e) s         = ...
-evalS_maybe (If e s1 s2) s     = ...
-
-Examples:
-
-
---EX5
-Here is a nice helpful monad that combines Maybe with a pending computation which requires a store to start processing.
-
-newtype Imperative a = Imperative {
-    runImperative :: Store -> Maybe (a, Store)
-}
-
-instance Monad Imperative where
-    return a = Imperative (\s -> Just (a,s))
-    b >>= f = Imperative (\s -> do (v1,s1) <- (runImperative b) s
-                                   runImperative (f v1) s1)
-    fail _ = Imperative (\s -> Nothing)
-
-Rewrite the evaluator in this monad:
-
-  evalE_monad :: Expression -> Imperative Value
-  evalS_monad :: Statement -> Imperative ()
-
-The following case is given to you:
-
-evalE_monad (BinOp o a b) = do a' <- evalE_monad a
-                               b' <- evalE_monad b
-                               return (applyOp o a' b')
-
-You still have to write:
-
-evalE_monad :: Expression -> Imperative Value
-evalE_monad (Var x) = ..
-evalE_monad (Val v) = ...
-evalE_monad (Assignment x e) = ...
-
-Similarly for statements
-
-evalS_monad :: Statement -> Imperative ()
-evalS_monad (While e s1)     = ...
-evalS_monad Skip             = ...
-evalS_monad (Sequence s1 s2) = ...
-evalS_monad (Expr e)         = ...
-evalS_monad (If e s1 s2)     = ...
-
-For the assignment and variable references, you need to return a monad that actually accesses the store as a map. It might help to define two function getVar and setVar first that do this kind of "dirty work".
-
-getVar :: Variable -> Imperative Value
-getVar var = Imperative (\store -> ((Data.Map.lookup var store) >>= (\v -> Just (v,store))))
-
-setVar :: Variable -> Value -> Imperative Value
-setVar var val = Imperative (\store -> Just (val, Data.Map.insert var val store))
-
-By using these methods, the rest of your code should consist of clean and easy to understand "do" blocks. The following example creates a monad called "miniprog" that basically does "x = 2; y = 3; return 2+3;":
-
-miniprog :: Imperative Value
-miniprog = do
-            setVar "x" (IntVal 2)
-            setVar "y" (IntVal 3)
-            a <- getVar "x"
-            b <- getVar "y"
-            return (applyOp Plus a b)
-
-You can run this monad with an empty initial store with
-
-> runImperative miniprog empty
-Just (IntVal 5,fromList [("x",2),("y",3)])
-
-Uncomment the function runMonad in the provided code to run the following examples:
-
-
---EX6
-Extend the Statement data type and the parser by adding a for loop to the language.  The syntax is
-
-for var in e1 to e2 do s end
-
-where var is a variable name, e1 and e2 are expressions and s is a statement.
-
-The semantics of this for loop is that the variable with the name var will be set to an initial value given by whatever the expression e1 returns and will execute the body s repeatedly until the variable var is greater than the value returned by expression e2.
-
-In addition to parsing, you also have to implement show, evalS, evalS_maybe and evalS_monad for your new loop.
-
-Examples:
-
--}
+        ,show (For "a" (Val (IntVal 1)) (Val (IntVal 2)) (Expr (Val (IntVal 3)))) ~?= "for a in 1 to 2 do 3 end"
+        ,show (parse stmtParser "" "for x in 1 to 4 do y = x end") ~?= "Right for x in 1 to 4 do y = x end"
+        ,r' stmtParser "for x in 1 to 4 do y = x end" evalS ~?= fromList [("x",IntVal 5),("y",IntVal 4)]
+        ,r' stmtParser "for x in 1 to 4 do y = x end" evalS_maybe ~?= Just (fromList [("x",IntVal 5),("y",IntVal 4)])
+        ,r' stmtParser "for x in 1 to 4 do y = z end" evalS_maybe ~?= Nothing
+        ,runMonad "for x in 1 to 4 do y = x end" ~?= Just (fromList [("x",IntVal 5),("y",IntVal 4)])
+        ,runMonad "for x in 1 to 4 do y = z end" ~?= Nothing
+        ]
